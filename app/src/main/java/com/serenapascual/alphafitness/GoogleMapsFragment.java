353f4ca27c635da1aa118719ced664f0c041f18c
serenapascual <serena.a.pascual@gmail.com>
2018-05-02 04:11:56 -0700

    Retrieve current location

--- app/src/main/java/com/serenapascual/alphafitness/GoogleMapsFragment.java
+++ app/src/main/java/com/serenapascual/alphafitness/GoogleMapsFragment.java
@@ -1,17 +1,32 @@
 package com.serenapascual.alphafitness;
 
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class GoogleMapsFragment extends Fragment  implements OnMapReadyCallback {
@@ -40,6 +55,70 @@ public class GoogleMapsFragment extends Fragment  implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        LocationManager locationManager;
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Get the best location provider
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy((Criteria.ACCURACY_FINE));
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String locationProvider = locationManager.getBestProvider(criteria, true);
+
+        // Make sure that the permission is granted
+        if (ActivityCompat.checkSelfPermission(this.getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this.getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Request missing permissions and case where user grants permission
+            return;
+        }
+
+        // Get the last known location
+        Location location = locationManager.getLastKnownLocation(locationProvider);
+
+        // Get the geocode of the given location
+        String label = "Address: ";
+        List<Address> addresses;
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                    }
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                    }
+                    @Override
+                    public void onLocationChanged(final Location location) {
+                    }
+                });
+
+        try {
+            Geocoder geocoder = new Geocoder(this.getContext(), Locale.getDefault());
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null) {
+                Address address = addresses.get(0);
+                StringBuilder stringBuilder = new StringBuilder("");
+                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                   stringBuilder.append(address.getAddressLine(i)).append("/");
+                   label = label + stringBuilder.toString();
+                }
+            }
+        }
+        catch (IOException e) {
+
+        }
+
+        // Add address and center map camera to given location
+        LatLng here = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(here).title(label));
+        mMap.moveCamera((CameraUpdateFactory.newLatLng(here)));
+
     }
 }
--- app/src/main/java/com/serenapascual/alphafitness/ProfileActivity.java
+++ app/src/main/java/com/serenapascual/alphafitness/ProfileActivity.java
@@ -1,13 +1,88 @@
 package com.serenapascual.alphafitness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        final EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        final EditText weightEditText = (EditText) findViewById(R.id.weightEditText);
+
+        // Disable blinking cursor when EditText not in focus
+        // Re-enable when EditText is in focus
+        nameEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == nameEditText.getId()) {
+                    nameEditText.setCursorVisible(true);
+                }
+            }
+        });
+
+        // Lose focus when user hits enter key
+        nameEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    nameEditText.setFocusable(false);
+                    nameEditText.setFocusableInTouchMode(true);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        // Set constant text " lbs" to end of user-editable weight
+        weightEditText.setText("170 lbs");
+        Selection.setSelection(weightEditText.getText(), weightEditText.getText().length());
+        weightEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().endsWith(" lbs")) {
+                    weightEditText.setText(" lbs");
+                    Selection.setSelection(weightEditText.getText(), weightEditText.getText().length());
+                }
+            }
+        });
+
+        // Lose focus when user hits enter key
+        weightEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    weightEditText.setFocusable(false);
+                    weightEditText.setFocusableInTouchMode(true);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
     }
 }
